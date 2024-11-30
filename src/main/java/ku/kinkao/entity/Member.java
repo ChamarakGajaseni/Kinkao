e2610363941d05d1840c755dcfc70e9788046acc
chamarak <chamarak.g@ku.th>
2024-11-30 18:58:35 +0700

    updated database and validation

--- src/main/java/ku/kinkao/entity/Member.java
+++ src/main/java/ku/kinkao/entity/Member.java
@@ -2,15 +2,21 @@ package ku.kinkao.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.Data;
 @Data
 @Entity
 public class Member {
     @Id
     @GeneratedValue
-    private int id;
+    private UUID id;
+
+//    private int id;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
+    private String email;
+    private Instant createdAt;
 }
--- src/main/java/ku/kinkao/repository/MemberRepository.java
+++ src/main/java/ku/kinkao/repository/MemberRepository.java
@@ -3,8 +3,10 @@ package ku.kinkao.repository;
 import ku.kinkao.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member,Integer> {
+public interface MemberRepository extends JpaRepository<Member,UUID> {
     Member findByUsername(String username);
 }
